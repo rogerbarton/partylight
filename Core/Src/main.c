792936c038f8557a599425768009f61fb3a0b84a
Roger Barton <rbarton@student.ethz.ch>
2023-03-21 17:13:16 +0100

    toggle pin

--- Core/Src/main.c
+++ Core/Src/main.c
@@ -105,8 +105,9 @@ int main(void)
   /* USER CODE BEGIN WHILE */
   while (1)
   {
+      HAL_GPIO_TogglePin(LED0_GPIO_Port, LED0_Pin);
+      HAL_Delay(100);
     /* USER CODE END WHILE */
-
     /* USER CODE BEGIN 3 */
   }
   /* USER CODE END 3 */
